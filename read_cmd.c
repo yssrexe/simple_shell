539516aecc3ef3730e69e6c064e3ba8aa201be1c
yssrexe <elyasiniyassir40@gmail.com>
2023-12-27 23:53:11 +0100

    betty

--- read_cmd.c
+++ read_cmd.c
@@ -1,19 +1,22 @@
 #include "shell.h"
-
-char *read_cmd(void)
+/**
+ * read_cmd - read commands
+ * Return: return cmd
+ */
+char	*read_cmd(void)
 {
-    size_t len = 0;
-    char *l = NULL;
-    ssize_t n;
+	size_t	len = 0;
+	char	*l = NULL;
+	ssize_t	n;
 
-    if (isatty(0))
-        write(1, "$ ", 2);
+	if (isatty(0))
+		write(1, "$ ", 2);
 
-    n = getline(&l, &len, stdin);
-    if (n == -1)
-    {
-        free(l);
-        return NULL;
-    }
-    return (l);
+	n = getline(&l, &len, stdin);
+	if (n == -1)
+	{
+		free(l);
+		return (NULL);
+	}
+	return (l);
 }
