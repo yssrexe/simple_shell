5155f2434ef11efaff66366d7921623a2196bb2e
yssrexe <elyasiniyassir40@gmail.com>
2023-12-28 00:19:35 +0100

    betty

--- functions_2.c
+++ functions_2.c
@@ -8,27 +8,26 @@
 
 int	fn_atoi(const char *str)
 {
-    int i = 0;
-    int sign = 1;
-    int result = 0;
-    while (str[i] == ' ' || str[i] == '\t')
-        i++;
+	int	i = 0;
+	int	sign = 1;
+	int	result = 0;
 
-    if (str[i] == 45 || str[i] == 43)
-    {
-        if (str[i] == 45)
-        {
-            sign=-1;
-        }
-        i++;
-    }
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
 
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        result = result * 10 + (str[i] - '0');
-        i++;
-    }
-    return result * sign;
+	if (str[i] == 45 || str[i] == 43)
+	{
+		if (str[i] == 45)
+			sign = -1;
+		i++;
+	}
+
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
 }
 
 /**
