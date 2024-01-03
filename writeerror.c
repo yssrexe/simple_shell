0c74090a1bb8cf746d026e246d0737f157d720ec
probrahim <ibrahimhassani674@gmail.com>
2024-01-03 01:57:13 +0100

    1

--- writeerror.c
+++ writeerror.c
@@ -1,15 +1,15 @@
 #include "shell.h"
 
-void writerror(char *sm, char *command ,int ind)
+void writerror(char *sm, char *command, int ind)
 {
-    char *ken = fn_itoa(ind);
+	char *ken = fn_itoa(ind);
 
-    fn_putstr(sm);
-    fn_putstr(": ");
-    fn_putstr(ken);
-    fn_putstr(": ");
-    fn_putstr(command);
-    fn_putstr(": not found\n");
-
-    free(ken);
+	fn_putstr(sm);
+	fn_putstr(": ");
+	fn_putstr(ken);
+	fn_putstr(": ");
+	fn_putstr(command);
+	fn_putstr(": not found\n");
+	free(ken);
 }
+
